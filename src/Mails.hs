3ea91d61cd26617030134928844d68d0f433ee3f
Sylvain HENRY <hsyl20@gmail.com>
2014-02-17 16:56:47 +0100

    Parse emails

--- src/Mails.hs
+++ src/Mails.hs
@@ -1,13 +1,22 @@
 import Network.HaskellNet.IMAP.SSL
 import Network.HaskellNet.IMAP
+import Network.HaskellNet.IMAP.Connection
+import Network.HaskellNet.IMAP.Types
 
 import Data.Traversable
 import Data.Foldable
+import Data.Maybe (catMaybes)
 import Control.Monad
 import Control.Applicative ((<$>))
 import Control.Exception
 
 import System.IO
+
+import Text.ParserCombinators.Parsec.Rfc2822 as Mail
+import Text.ParserCombinators.Parsec
+
+import qualified Data.ByteString.Char8 as BS
+
 
 main :: IO ()
 main = do
@@ -28,9 +37,24 @@ main = do
    select conn "INBOX"
 
    uids <- search conn [ALLs]
+
+   traverse_ (showHeader conn) uids
    
-   hdr <- fetch conn (head uids)
-   putStrLn (show hdr)
+   logout conn
+
+showHeader :: IMAPConnection -> UID -> IO ()
+showHeader conn uid = do
+   content <- fetch conn uid
+
+   let 
+      (Right (Message fields body)) = parse Mail.message "" (BS.unpack content)
+      subject = head . catMaybes $ fmap extractSubject fields
+      extractSubject (Subject x) = Just x
+      extractSubject _ = Nothing
+
+   putStrLn ("Subject: "++ subject)
+   --putStrLn ("Body: "++ body)
+
 
 withEcho :: Bool -> IO a -> IO a
 withEcho echo action = do
